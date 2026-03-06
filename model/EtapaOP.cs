e7c9602e869da1713d02c5341b5f1b1495d1d661
Victor Gabriel <vkriok@gmail.com>
2026-03-06 09:29:08 -0300

    Update EtapaOP.cs

--- model/EtapaOP.cs
+++ model/EtapaOP.cs
@@ -3,21 +3,22 @@ namespace api.Model
     public class EtapaOP
     {
         public Guid Id{get;set;}
-        public int Op_id{get;set;}
-        public int Etapa_id{get;set;}
+        public int OpId{get;set;}
+        public int EtapaId{get;set;}
         public DateTime Data_Inicio{get;set;}
         public DateTime Data_Fim{get;set;}
         public string Status{get;set;}
         public OrdemProducao OrdemProducao{get;set;}
 
-        public EtapaOP(int op_Id, int etapa_Id,DateTime data_inicio, DateTime data_fim, string status)
+        public EtapaOP(int opId, int etapa_Id,DateTime data_inicio, DateTime data_fim, string status)
         {
             Id = Guid.NewGuid();
-            Op_id = op_Id;
-            Etapa_id = etapa_Id;
+            Opid = opId;
+            EtapaId = etapaId;
             Data_Inicio = data_inicio;
             Data_Fim = data_fim;
             Status = status;
         }
     }
+
 }
