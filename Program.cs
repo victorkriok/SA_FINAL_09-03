85179a5de5492cc2435ed73eaeec393200342160
Bernardo Fernandes <bernardofpdev@gmail.com>
2026-03-07 23:14:21 -0300

    feat: configurando db, jwt, e chamando endpoints

--- Program.cs
+++ Program.cs
@@ -1,8 +1,35 @@
+using api.data;
+using Microsoft.EntityFrameworkCore;
+using api.routes;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // registrar serviços
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApiContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+var key = "chave_super_secreta_aqui_123456";
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(key))
+    };
+});
 
 var app = builder.Build();
 
@@ -14,4 +41,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UsuarioRoute();
+app.PerfilRoutes();
 app.Run();
