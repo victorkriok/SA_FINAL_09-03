47234d6b95982e5367ed8a5f591c0e86c9a36037
Bernardo Fernandes <bernardofpdev@gmail.com>
2026-03-07 23:13:16 -0300

    feat: criando service para utilizar hash e salt

---
+++ service/PasswordService.cs
@@ -0,0 +1,16 @@
+using BCrypt.Net;
+
+namespace api.security;
+
+public static class PasswordService
+{
+    public static string HashPassword(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+    public static bool VerifyPassword(string password, string hash)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, hash);
+    }
+}
