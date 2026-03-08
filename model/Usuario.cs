7e9a05d7fdeffe6d715cdc610e98d97894dd2daa
Bernardo Fernandes <bernardofpdev@gmail.com>
2026-03-07 23:10:43 -0300

    feat: adicionando hash e salt

--- model/Usuario.cs
+++ model/Usuario.cs
@@ -6,11 +6,14 @@ public class Usuario
     public string Nome { get; set; }
     public string Login { get; set; }
     public string SenhaHash { get; set; }
-    public int PerfilId { get; set; }
+
+    public Guid PerfilId { get; set; }
+    public Perfil Perfil { get; set; }
+
     public bool Ativo { get; set; }
     public DateTime CriadoEm { get; set; }
 
-    public Usuario(string nome, string login, string senhaHash, int perfilId)
+    public Usuario(string nome, string login, string senhaHash, Guid perfilId)
     {
         Id = Guid.NewGuid();
         Nome = nome;
@@ -18,7 +21,6 @@ public class Usuario
         SenhaHash = senhaHash;
         PerfilId = perfilId;
         Ativo = true;
-        CriadoEm = DateTime.Now;
-        
+        CriadoEm = DateTime.UtcNow;
     }
 }
